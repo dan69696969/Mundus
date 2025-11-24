d2b316fe8123448163bd7645892b3cd3cc69d961
dan69696969 <macoun.daniel@seznam.cz>
2025-11-24 20:37:26 +0100

    nedodelaný health
    jespes

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private void Awake()
     {
-        
+        currentHealth = startingHealth;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(float _damage)
     {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
         
+        if(currentHealth > 0)
+        {
+            //player hurt
+        }
+        else
+        {
+            //player dead
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+            TakeDamage(1);
     }
 }
